d018d7e86f42f5f23cb9e362e83d1cc758e4c463
vishal <vishal.patel77851@gmail.com>
2016-06-29 15:26:15 +0530

    commmm

--- app/src/main/java/com/example/vishal/createcsv/MainActivity.java
+++ app/src/main/java/com/example/vishal/createcsv/MainActivity.java
@@ -1,40 +1,87 @@
 package com.example.vishal.createcsv;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.dropbox.core.android.Auth;
+import com.dropbox.core.v2.users.FullAccount;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    //
+    private static final String IMAGE_DIRECTORY_NAME = "MyData";
+    public static String timeStamp;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    File mediaFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<String> li1 = new ArrayList<String>(){
-            {
-                this.add("animal");
-                this.add("nuts");
-                this.add("java");
+
+        try {
+
+
+            File mediaStorageDir = new File(
+                    Environment
+                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    IMAGE_DIRECTORY_NAME);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    // MyApplication.getInstance().showLog("TAG", "Oops! Failed create " + IMAGE_DIRECTORY_NAME + " directory");
+
+                }
             }
-        };
+
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                    Locale.getDefault()).format(new Date());
 
 
-        System.out.println( getListAsCsvString(li1));
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "file_" + timeStamp + ".csv");
+            CSVWriter writer = new CSVWriter(new FileWriter(mediaFile));
+
+            List<String[]> data = new ArrayList<String[]>();
+            data.add(new String[]{"India", "New Delhi"});
+            data.add(new String[]{"United States", "Washington D.C"});
+            data.add(new String[]{"Germany", "Berlin"});
+
+            writer.writeAll(data);
+
+            writer.close();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
 
     }
 
-    public String getListAsCsvString(List<String> list){
+    public String getListAsCsvString(List<String> list) {
 
         StringBuilder sb = new StringBuilder();
-        for(String str:list){
-            if(sb.length() != 0){
+        for (String str : list) {
+            if (sb.length() != 0) {
                 sb.append(",");
             }
             sb.append(str);
         }
         return sb.toString();
     }
+
+
 }
